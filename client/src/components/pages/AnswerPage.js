abf198a0fee54bec84200b7126c3ef96c78dec09
Simon <simon.j.lee@stonybrook.edu>
2023-05-16 18:38:29 -0400

    partially done with answerComp

--- client/src/components/pages/AnswerPage.js
+++ client/src/components/pages/AnswerPage.js
@@ -1,19 +1,71 @@
-import Model from '../../models/model'
-import { useState, useEffect } from 'react'
-import Answer from './answerComp'
-import React from 'react'
-import PropTypes from 'prop-types';
+import Model from "../../models/model";
+import { useState, useEffect } from "react";
+import Answer from "./answerComp";
+import React from "react";
+import axios from 'axios'
+import PropTypes from "prop-types";
 
-export default function AnswerPage ({ changeToPage, question_in }) {
-  console.log(question_in)
-  const [question, setCurrentQuestion] = useState(question_in)
-  useEffect(() => {
-    setCurrentQuestion(question)
-  }, [question])
-  const tagSideItem = document.querySelector('#tag')
-  tagSideItem.classList.remove('active')
-  const questionSideItem = document.querySelector('#question')
-  questionSideItem.classList.remove('active')
+
+
+export default function AnswerPage({ changeToPage, question_in }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const answersPerPage = 5;
+  // const [reputationError, setReputationError] = useState(false);
+  const [voteStatus, setVoteStatus] = useState('');
+
+  console.log(question_in);
+  const [question, setCurrentQuestion] = useState(question_in);
+  // useEffect(() => {
+  //   setCurrentQuestion(question);
+  // }, [question]);
+  const tagSideItem = document.querySelector("#tag");
+  tagSideItem.classList.remove("active");
+  const questionSideItem = document.querySelector("#question");
+  questionSideItem.classList.remove("active");
+
+  let tagsList = question.tags.map((tag) => (
+    <div className="small" key={tag._id}>
+      {`${tag.name}`}
+    </div>
+  ));
+
+  async function upvote_q() {
+    try {
+      const response = await axios.post(`http://localhost:8000/question/${question._id}/vote`, {
+        op: 'upvote',
+        switch: voteStatus === 'downvote',
+      });
+      if (response.data.status === 'SUCCESS') {
+        const res = await axios.get(`http://localhost:8000/questions/${question._id}`);
+        setCurrentQuestion(res.data);
+      } else if(response.data.status === "LOW-REPUTATION"){
+        alert("Your reputation must be 50 or higher to vote!");
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle error if necessary
+    }
+  }
+
+  async function downvote_q() {
+    try {
+      const response = await axios.post(`http://localhost:8000/question/${question._id}/vote`, {
+        op: 'downvote',
+        switch: voteStatus === 'upvote',
+      });
+      if (response.data.status === 'SUCCESS') {
+        const res = await axios.get(`http://localhost:8000/questions/${question._id}`);
+        console.log(res.data);
+        setCurrentQuestion(res.data);
+      } else if(response.data.status === "LOW-REPUTATION"){
+        alert("Your reputation must be 50 or higher to vote!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  let votes = question.upvote - question.downvote;
 
   // Create header element
   const header = (
@@ -21,41 +73,111 @@ export default function AnswerPage ({ changeToPage, question_in }) {
       <div className="headerQuestion">
         <div className="headerTitle">{`${question.answers.length} Answers`}</div>
         <div className="questionTitle">{question.title}</div>
-        <button className="headerButton" onClick={() => { changeToPage('questionModal') }}>Ask Question</button>
+        <button
+          className="headerButton"
+          onClick={() => {
+            changeToPage("questionModal");
+          }}
+        >
+          Ask Question
+        </button>
       </div>
       <div className="headerQuestion">
-        <div className="headerNumber">{`${question.views} views`}</div>
-        <QText text = {question.text}/>
+        <div className="headerInfo">
+          <button id= "upvote" className="vote-button" onClick={upvote_q}>
+            <span className ="vote-icon">&#9650;</span>
+          </button>
+          <span
+              className ="vote-count"
+              id="upvote"
+            >{`Votes: ${votes}`}</span>
+          <button id="downvote" className ="vote-button" onClick={downvote_q}>
+            <span className ="vote-icon">&#9660;</span>
+          </button>
+          <div className="headerNumber">{`${question.views} views`}</div>
+        </div>
+        <QText text={question.text} />
         <div className="headerUser">
-          <div className="username">{question.asked_by}</div>
+          <div className="username">{question.asked_by.username}</div>
           <div className="date">{`asked ${Model.dateFormat(
             question.ask_date_time
           )}`}</div>
         </div>
+        <div className="questionTags">Tags: {tagsList}</div>
       </div>
     </div>
-  )
-  const answerItems = question.answers.map(answer =>
-    <div key = {answer._id}>
-      <Answer answer = {answer} />
-    </div>
-  )
+  );
+  let answerItems; // question will changes based on amount.
+  //  if (typeof questions == "undefined" || questions.length == 0) {
+  if (question.answers.length === 0) {
+    answerItems = (
+      <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>
+        No Answers
+      </p>
+    );
+  } else {
+    // console.log(questions);
+    var indexOfLastAnswer = currentPage * answersPerPage;
+    var indexOfFirstAnswer = indexOfLastAnswer - answersPerPage;
+    var currentAnswers = question.answers.slice(
+      indexOfFirstAnswer,
+      indexOfLastAnswer
+    );
+    answerItems = currentAnswers.map((answer) => (
+      <div key={answer._id}>
+        <Answer ans={answer} />
+      </div>
+    ));
+  }
+  const goToNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = indexOfLastAnswer >= question.answers.length;
+
   return (
     <>
+      
       {header}
       <div className="content-container">
         {answerItems}
-        <button className="botButton" id="postAnswer" onClick={() => changeToPage('answerModal', { question })}>
+        <div className="pagination-buttons">
+        <button className="ordering" disabled={isFirstPage} onClick={goToPreviousPage}>
+          Prev
+        </button>
+        <button className="ordering" disabled={isLastPage} onClick={goToNextPage}>
+          Next
+        </button>
+        <p>{indexOfFirstAnswer}......{indexOfLastAnswer}</p>
+      </div>
+        <button
+          className="botButton"
+          id="postAnswer"
+          onClick={() => changeToPage("answerModal", { question })}
+        >
           Answer Question
         </button>
       </div>
     </>
-  )
+  );
 }
 const QText = ({ text }) => {
-  const convertedText = text.replace(/\[([^\]]+)\]\((.*?)\)/g, '<a href="$2" target = "_blank">$1</a>')
-  return <div className="questionText" dangerouslySetInnerHTML={{ __html: convertedText }} />
-}
+  const convertedText = text.replace(
+    /\[([^\]]+)\]\((.*?)\)/g,
+    '<a href="$2" target = "_blank">$1</a>'
+  );
+  return (
+    <div
+      className="questionText"
+      dangerouslySetInnerHTML={{ __html: convertedText }}
+    />
+  );
+};
 
 AnswerPage.propTypes = {
   changeToPage: PropTypes.func.isRequired,
@@ -63,5 +185,5 @@ AnswerPage.propTypes = {
 };
 
 QText.propTypes = {
-  text: PropTypes.string.isRequired
-}
+  text: PropTypes.string.isRequired,
+};
--- client/src/components/pages/answerComp.js
+++ client/src/components/pages/answerComp.js
@@ -1,15 +1,85 @@
 import Model from '../../models/model'
 import PropTypes from 'prop-types';
 import React from 'react';
+import axios from 'axios'
+import { useState, useEffect } from "react";
 // import { useState } from "react";
 
 export default function Answer (props) {
-  const { answer } = props
+    const { ans } = props
+  // const real = await axios.get(`http://localhost:8000/answers/${ans._id}`);
+    // console.log(ans);
+    const [answer, setCurrentAnswer] = useState(ans);
+    const [voteStatus, setVoteStatus] = useState('');
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`http://localhost:8000/answers/${ans._id}`);
+          const real = response.data;
+          setCurrentAnswer(real);
+          console.log(real);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchData();
+    }, []);
+  useEffect(() => {
+    setCurrentAnswer(answer);
+  }, [answer]);
+  async function upvote_a() {
+    try {
+      const response = await axios.post(`http://localhost:8000/answer/${answer._id}/vote`, {
+      op: 'upvote',
+      switch: voteStatus === 'downvote',
+    })
+      if (response.data.status === 'SUCCESS') {
+        const res = await axios.get(`http://localhost:8000/answers/${answer._id}`);
+        setCurrentAnswer(res.data);
+      } else if(response.data.status === "LOW-REPUTATION"){
+        alert("Your reputation must be 50 or higher to vote!");
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle error if necessary
+    }
+  }
+
+  async function downvote_a() {
+    try {
+      const response = await axios.post(`http://localhost:8000/answer/${answer._id}/vote`, {
+        op: 'downvote',
+        switch: voteStatus === 'upvote',
+      });
+      if (response.data.status === 'SUCCESS') {
+        const res = await axios.get(`http://localhost:8000/answers/${answer._id}`);
+        setCurrentAnswer(res.data);
+      } else if(response.data.status === "LOW-REPUTATION"){
+        alert("Your reputation must be 50 or higher to vote!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  let votes = answer.upvote.length - answer.downvote.length;
+
   return (
     <div className="answer">
+      <div className="headerInfo">
+          <button id= "upvote" className="vote-button" onClick={upvote_a}>
+            <span className ="vote-icon">&#9650;</span>
+          </button>
+          <span
+              className ="vote-count"
+              id="upvote"
+            >{`Votes: ${votes}`}</span>
+          <button id="downvote" className ="vote-button" onClick={downvote_a}>
+            <span className ="vote-icon">&#9660;</span>
+          </button>
+      </div>
       <AnswerText text = {answer.text}/>
       <div className="user-container">
-        <div className="username">{answer.ans_by}</div>
+        <div className="username">{answer.ans_by.username}</div>
         <div className="date">{`answered ${Model.dateFormat(
           answer.ans_date_time
         )}`}</div>
@@ -17,13 +87,13 @@ export default function Answer (props) {
     </div>
   )
 }
-Answer.propTypes = {
-  answer: PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    ans_by: PropTypes.string.isRequired,
-    ans_date_time: PropTypes.string.isRequired
-  }).isRequired
-};
+// Answer.propTypes = {
+//   answer: PropTypes.shape({
+//     text: PropTypes.string.isRequired,
+//     ans_by: PropTypes.string.isRequired,
+//     ans_date_time: PropTypes.string.isRequired
+//   }).isRequired
+// };
 
 const AnswerText = ({ text }) => {
   const convertedText = text.replace(/\[([^\]]+)\]\((.*?)\)/g, '<a href="$2" target = "_blank">$1</a>')
